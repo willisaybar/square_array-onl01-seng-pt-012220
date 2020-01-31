347138b06fefb8d3af1a07386b04f7efbb3e98cc
Willis Aybar <willisaybar@gmail.com>
2020-01-31 16:32:36 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -7,16 +7,27 @@
 #   end
 # end
 
-def square_array(array)
-   new_array = []
-   counter = 0 
-   while counter < array.length do
-     new_array << array[counter] ** 2
-     counter += 1 
-    #counter = counter + 1
-   end
-   ##return the new array
-   new_array
-end
+# def square_array(array)
+#   new_array = []
+#   counter = 0 
+#   while counter < array.length do
+#     new_array << array[counter] ** 2
+#     counter += 1 
+#     #counter = counter + 1
+#   end
+#   ##return the new array
+#   new_array
+# end
 
-numbers = [1,2,3]
+# numbers = [1,2,3]
+
+
+def square_array(array)
+  square_array = [1,2,3]
+  counter = 0
+  square_array.each do |number|
+  new_array << number
+  counter ** 2
+  end
+  new_array
+end
