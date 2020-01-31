cc767944c3eeee735e086a02bc51442d0fbfa6e9
Willis Aybar <willisaybar@gmail.com>
2020-01-31 14:31:31 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,14 @@
+numbers = ["1", "2", "3"]
+
 def square_array(array)
-  # your code here
+  numbers.each do |number|
+    puts {|number| * 2}
+    new_array << number * 2 
+  end
 end
+
+# def square_array(array)
+#   array.each do {|array| * 2}
+# end
+
+#   new_array << number * 2 
