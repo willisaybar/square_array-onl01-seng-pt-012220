3bf13fccddb2250cd3d303243f9793d50a780dbd
Willis Aybar <willisaybar@gmail.com>
2020-01-31 15:55:39 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -10,7 +10,7 @@
 def square_array(array)
    new_array = []
    counter = 0 
-   each.do counter < array.length do
+   while counter < array.length do
      new_array << array[counter] ** 2
      counter += 1 
     #counter = counter + 1
